fd6a6812a7cc680627a3d80ee0b15679b6757860
vinayvinu <tink2hakk@gmail.com>
2024-05-07 01:00:40 +1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+let age = 25;
+console.log(age);
+ let a = 1
+ let b = "abc"
+ let c = 0.1
+ let d = True;
+ let e = null;
+
+ console.log(typeof(a))
+ console.log(typeof(b))
+ console.log(typeof(c))
+ console.log(typeof(d))
+ console.log(typeof(e))
+
